9f7d07027ad138e3d7aa32ff571f26b8bd9e5852
raguiarperez <raguiarperez@nautilus8.danielcastelao.org>
2019-01-24 09:21:27 +0100

    Boletin 19_1

---
+++ Boletin19_1/src/boletin19_1/Boletin19_1.java
@@ -0,0 +1,16 @@
+package boletin19_1;
+
+/**
+ * @author raguiarperez
+ */
+public class Boletin19_1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Metodos obx = new Metodos();
+        obx.generarNumeros();
+    }
+    
+}
